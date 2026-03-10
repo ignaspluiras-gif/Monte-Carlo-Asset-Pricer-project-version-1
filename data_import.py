8ecb3fa0d93839fc08f42c3a0f0d2a79477fad0e
ignaspluiras-gif <ignaspluiras@gmail.com>
2026-03-10 11:29:35 +0100

    Refactor comments for clarity in data_import.py

--- data_import.py
+++ data_import.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 def get_stock_data(ticker, start_date, end_date):
-    # this line starts definition of the function for pulling out the needed data from yahoo finance data bank
+    # pulling out the needed data from yahoo finance data bank
     data = yf.download(ticker, start=start_date, end=end_date)
     
     # this line extracts the closing price of stock, but also takes into consideration that the Stock might not have adjusted price for some period of time
@@ -13,14 +13,14 @@ def get_stock_data(ticker, start_date, end_date):
         prices = data['Close']
     
 
-    # this calculates sum of daily returns of the stock and puts them in log function for numerical stability
+    # this calculates sum of daily returns of the stock and puts them in log function to get rid of multiplication
     log_returns = np.log(1 + prices.pct_change())
     
     return prices, log_returns
 
 # this is preperation for Normal distribution function, we calulate two needed elements: variance and the mean
 # mean  represents  average direction of the stocks path over time (does it go up down or stays flat and by how much).
-#variance measures how "spread out" the jumps are from the average, taking the absolute value
+#variance measures how "spread out" the jumps are from the average in the absolute value
 def get_stats(log_returns):
     # Calculates drift and variance
     u = log_returns.mean()
